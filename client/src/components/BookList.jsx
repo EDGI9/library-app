a005ec3dd27895fbf45cebab9502b81791a32570
Edgar Dias <dias.s.edgar@gmail.com>
2024-09-05 09:00:56 +0000

    Create initial compoent for book list and book component

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,41 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import booksAPI from "../apis/books";
+import Book from "./Book";
 
-
-const Book = (props) => (
-  <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-    <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-      {props.book.name}
-    </td>
-    <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-      {props.book.position}
-    </td>
-    <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-      {props.book.level}
-    </td>
-    <td className="p-4 align-middle [&amp;:has([role=checkbox])]:pr-0">
-      <div className="flex gap-2">
-        <Link
-          className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3"
-          to={`/edit/${props.book._id}`}
-        >
-          Edit
-        </Link>
-        <button
-          className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 hover:text-accent-foreground h-9 rounded-md px-3"
-          color="red"
-          type="button"
-          onClick={() => {
-            props.deleteBook(props.book._id);
-          }}
-        >
-          Delete
-        </button>
-      </div>
-    </td>
-  </tr>
-);
 
 export default function BookList() {
   const [books, setBooks] = useState([]);
@@ -79,32 +46,19 @@ export default function BookList() {
 
   return (
     <>
-      <h3 className="text-lg font-semibold p-4">Book Records</h3>
-      <div className="border rounded-lg overflow-hidden">
-        <div className="relative w-full overflow-auto">
-          <table className="w-full caption-bottom text-sm">
-            <thead className="[&amp;_tr]:border-b">
-              <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-                <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
-                  Name
-                </th>
-                <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
-                  Position
-                </th>
-                <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
-                  Level
-                </th>
-                <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0">
-                  Action
-                </th>
-              </tr>
-            </thead>
-            <tbody className="[&amp;_tr:last-child]:border-0">
-              {bookList()}
-            </tbody>
-          </table>
-        </div>
-      </div>
+      <section className="flex justify-center">
+          <div className="w-[80%] flex flex-wrap justify-center md:justify-start">
+            {books.map(item => {
+              return (
+                <Book
+                  book={item}
+                  deleteBook={() => deleteBook(item._id)}
+                  key={item._id}
+                />
+            )
+            })}
+          </div>
+      </section>
     </>
   );
 }
