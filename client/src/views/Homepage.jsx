b12a612ece5390562fee6d009f3340ba95c7c971
Edgar Dias <dias.s.edgar@gmail.com>
2024-09-12 09:59:07 +0000

    Creation of a homepage

---
+++ client/src/views/Homepage.jsx
@@ -0,0 +1,31 @@
+import BookList from "../components/BookList.jsx";
+import Counter from "../components/Counter.jsx";
+
+const Homepage = () => {
+    return (
+        <div className="flex flex-col md:grid auto-rows-max grid-cols-12 gap-4">
+            <section className="bg-slate-600 row-start-1 col-span-12 h-[600px]">
+                Message
+            </section>
+            <section className="md:grid grid-rows-1 grid-cols-12 row-start-2 col-span-12 gap-4 bg-yellow-300 ">
+                <section className="md:grid auto-rows-max bg-blue-600 row-start-1 col-span-8 ">
+                    <div className="flex items-center justify-center row-start-1 gap-4">
+                        <BookList></BookList>
+                    </div>
+                    <div className="flex flex-col md:flex-row items-center justify-center bg-green-600 row-start-2 gap-4">
+                        <Counter icon="asdasd" number="1" text="My text"></Counter>
+                        <Counter icon="asdasd" number="2" text="My text"></Counter>
+                        <Counter icon="asdasd" number="3" text="My text"></Counter>
+                    </div>
+                </section>
+                <section className="bg-red-600 row-start-1 col-span-4">Book Recomendation</section>
+            </section>
+            <section className="bg-blue-300 row-start-3 col-span-12 h-[400px]">
+                Author Highlight
+            </section>
+
+        </div>
+    )
+}
+
+export default Homepage;
