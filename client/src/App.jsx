d2241d1f4359d13ca2dcc3a7c5b93b40c0d8cb26
Edgar Dias <dias.s.edgar@gmail.com>
2024-09-14 16:34:48 +0000

    Removal of code on App that fetched all books

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,8 @@
 import { Outlet } from "react-router-dom";
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from "react";
-
-import { GET_ALL_BOOKS } from "./store/slices/books";
 
 import Navbar from "./components/Navbar";
 
 const App = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    async function getBooks() {
-      dispatch(GET_ALL_BOOKS())
-    }
-    getBooks();
-    
-    return;
-  }, [dispatch]);
 
   return (
     <div className="w-full p-6">
