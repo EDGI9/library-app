1b0e0f7c433bf9469b7e71e7d5f0e40be90f9c22
EDGI9 <dias.s.edgar@gmail.com>
2024-09-20 08:42:02 +0000

    Adding solution to animate elements on page load without the use of packages

--- client/src/views/Homepage.jsx
+++ client/src/views/Homepage.jsx
@@ -6,6 +6,7 @@ import Counter from "../components/Counter.jsx";
 import Book from "../components/Book.jsx";
 
 import homepageImage from '../assets/library-homepage-image.webp';
+import "../utils/tailwind-animation.js";
 
 import { GET_ALL_BOOKS, } from "../store/slices/books";
 import { DELETE_BOOK } from '../store/slices/book';
@@ -38,8 +39,11 @@ const Homepage = () => {
             <section className="md:grid grid-rows-1 grid-cols-12 row-start-1 col-span-12 h-[200px]  md:h-[600px]">
                 <div className='flex flex-col md:flex-row items-center justify-center gap-4 col-start-2 col-span-10'>
                     <div>
-                        <h1 className='flex flex-col lg:flex-row lg:space-x-3 text-5xl'><span>The</span> <b className='text-primary'>Bookshelf Bliss</b></h1>
-                        <h6 className='text-sm'>Browse for books at anytime</h6>
+                        <h1 className='flex flex-col lg:flex-row lg:space-x-3 text-5xl'>
+                            <span className='transform transition-all opacity-0 translate-y-12 ease-out duration-700 delay-200' data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>The</span>
+                            <b className='text-primary transform transition-all opacity-0 -translate-y-12 ease-out duration-700 delay-500' data-replace='{ "-translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>Bookshelf Bliss</b>
+                        </h1>
+                        <h6 className='transform transition-all opacity-0 -translate-x-12 ease-out duration-700 delay-1000 text-sm' data-replace='{ "-translate-x-12": "translate-x-0", "opacity-0": "opacity-100" }'>Browse for books at anytime</h6>
                     </div>
                     <div className='hidden md:flex flex-col '>
                         <img src={homepageImage} height={500} width={700} alt="" />
