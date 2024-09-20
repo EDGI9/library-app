bd5d33ac33b5d51856e88d469156b89286f8cbba
EDGI9 <dias.s.edgar@gmail.com>
2024-09-20 07:53:24 +0000

    Improve Homepage UI by adding logos, images, colors and adding html elements to page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import { NavLink } from "react-router-dom";
 
+import logoImage from '../assets/logo.webp';
+
 const Navbar = () => {
     return (
         <div>
             <nav className="flex justify-between istems-center mb-6">
                 <NavLink to="/">
-                    <img alt="MongoDB logo" className="h-10 inline" src="https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/mongodb/mongodb-original-wordmark.svg"></img>
+                    <img alt="MongoDB logo" className="h-10 inline" src={logoImage}></img>
                 </NavLink>
 
-                <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3" to="/create">
+                <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-primary transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-primary text-white hover:text-primary hover:bg-slate-100 h-9 rounded-md px-3" to="/create">
                     Add Book
                 </NavLink>
             </nav>
--- client/src/views/Homepage.jsx
+++ client/src/views/Homepage.jsx
@@ -5,6 +5,8 @@ import BookList from "../components/BookList.jsx";
 import Counter from "../components/Counter.jsx";
 import Book from "../components/Book.jsx";
 
+import homepageImage from '../assets/library-homepage-image.webp';
+
 import { GET_ALL_BOOKS, } from "../store/slices/books";
 import { DELETE_BOOK } from '../store/slices/book';
 
@@ -33,8 +35,17 @@ const Homepage = () => {
 
     return (
         <div className="flex flex-col md:grid auto-rows-max grid-cols-12 gap-4">
-            <section className="bg-slate-600 row-start-1 col-span-12 h-[600px]">
-                Message
+            <section className="md:grid grid-rows-1 grid-cols-12 row-start-1 col-span-12 h-[200px]  md:h-[600px]">
+                <div className='flex flex-col md:flex-row items-center justify-center gap-4 col-start-2 col-span-10'>
+                    <div>
+                        <h1 className='flex flex-col lg:flex-row lg:space-x-3 text-5xl'><span>The</span> <b className='text-primary'>Bookshelf Bliss</b></h1>
+                        <h6 className='text-sm'>Browse for books at anytime</h6>
+                    </div>
+                    <div className='hidden md:flex flex-col '>
+                        <img src={homepageImage} height={500} width={700} alt="" />
+                        <a className='text-[5px] self-end me-[10%]' href="https://www.freepik.com/free-vector/people-library-flat-vector-illustration_9176169.htm#fromView=keyword&page=1&position=6&uuid=71b74823-d0bc-4681-bae3-397ded119558" target='_blank'>Image by pch.vector on Freepik</a>
+                    </div>
+                </div>
             </section>
             <section className="md:grid grid-rows-1 grid-cols-12 row-start-2 col-span-12 gap-4 bg-yellow-300 ">
                 <section className="md:grid auto-rows-max bg-blue-600 row-start-1 col-span-8 ">
