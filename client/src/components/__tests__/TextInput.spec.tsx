07b9b47b9156c3468f14f5875442454bced25153
EDGI9 <dias.s.edgar@gmail.com>
2024-09-26 15:33:53 +0000

    Adding unit test for TextInput component

---
+++ client/src/components/__tests__/TextInput.spec.tsx
@@ -0,0 +1,54 @@
+import { describe, it, expect, afterAll, beforeEach, vi } from "vitest";
+import { render, cleanup, RenderResult, fireEvent } from '@testing-library/react';
+import { faker } from "@faker-js/faker";
+import React from "react";
+
+import TextInput from "../TextInput.jsx";
+
+describe('TextInput component', () => {
+    let component: RenderResult;
+    let textInput: HTMLElement;
+    const props ={
+        name: faker.word.words(1),
+        placeholder: faker.word.words(2),
+        value: faker.word.words(2),
+        onChange: vi.fn(),
+    }
+   
+    beforeEach(()=> {
+        component = render(
+            <TextInput {...props} />
+        );
+        textInput = component.getByTestId('qa-text-input');
+    });
+
+    afterAll(() => {
+        cleanup();
+    });
+
+    it('Renders Component', () => {
+        expect(textInput).toBeTruthy();
+    });
+
+    it('Component has correct property values', () => {
+        const name = textInput.getAttribute('name');
+        const placeholder = textInput.getAttribute('placeholder');
+        const value = textInput.value;
+        
+        expect(name).toEqual(props.name);
+        expect(placeholder).toEqual(props.placeholder);
+        expect(value).toEqual(props.value);
+    });
+
+    it('Component is emits change', async () => {
+        const newValue = faker.word.words(1);  
+        const value = textInput.value;
+
+        expect(value).toEqual(props.value);
+
+        await fireEvent.change(textInput, { target: { value: newValue } });
+
+        expect(props.onChange).toHaveBeenCalledOnce();
+        expect(props.onChange).toHaveBeenCalledWith(newValue);
+    });
+});
