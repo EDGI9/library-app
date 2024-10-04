8e75f2f70b209c3c9d63bf2c36f36c877024a4a4
EDGI9 <dias.s.edgar@gmail.com>
2024-10-04 12:29:25 +0000

    Adding data test id to footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     const contactsList = ["email.email@email.com", faker.phone.number({ style: 'international' }), "Twitter", "Facebook", "Instagram"];
 
     return (
-        <footer className="flex justify-center bg-primary p-10 text-white mt-36">
+        <footer data-testid="qa-footer" className="flex justify-center bg-primary p-10 text-white mt-36">
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-3/5" >
                 <div className="grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1">
                     <div>
