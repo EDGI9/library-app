caf87e1ae58674dd5a6daaef7dd1954b84c009cb
EDGI9 <dias.s.edgar@gmail.com>
2024-10-04 15:22:40 +0000

    Adding new component to Gallery page

--- client/src/views/BookGalleryPage.jsx
+++ client/src/views/BookGalleryPage.jsx
@@ -2,62 +2,38 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useEffect, useState } from 'react';
 
 import BookList from "../components/BookList.jsx";
-import TextInput from "../components/TextInput.jsx";
+import Filters from "../components/Filters.jsx";
 
 import { GET_FILTERED_BOOKS} from "../store/slices/books";
 
 const BookGalleryPage = () => {
     const dispatch = useDispatch();
     const books = useSelector((state) => state.books.filteredItems);
-    let [filters, setFliters] = useState({});
+    let [filters, setFliters] = useState({
+        name: '',
+        author: '',
+        genre: '',
+    });
 
     useEffect(() => {
         getBooks()
     }, [filters]);
 
-    function getBooks() {
+    const getBooks = () => {
         dispatch(GET_FILTERED_BOOKS(filters))
     }
 
     const updateFilters = (value) => {
         return setFliters((prev) => {
             return { ...prev, ...value };
-          })
-      }
+        })
+    }
 
     return (
         <div className="grid grid-cols-3 grid-rows-1">
             <section className="max-w-96 bg-primary">
                 <h2>Filters (WIP)</h2>
-                <h3>Move filters to a component</h3>
-                <label
-                  htmlFor="name"
-                >
-                  Name
-                </label>
-                <div className="mt-2">
-                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-slate-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                    <TextInput 
-                      name="name"
-                      value={filters?.name}
-                      placeholder="Name" 
-                      onChange={(text) => updateFilters({ name: text})}/>
-                  </div>
-                </div>
-                <label
-                  htmlFor="name"
-                >
-                  Author
-                </label>
-                <div className="mt-2">
-                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-slate-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                    <TextInput 
-                      name="author"
-                      value={filters?.author}
-                      placeholder="Author" 
-                      onChange={(text) => updateFilters({ author: text})}/>
-                  </div>
-                </div>
+                {<Filters fields={filters} onChange={updateFilters}/>}
             </section>
             <section className='col-span-2'>
                 <BookList books={books}></BookList>
