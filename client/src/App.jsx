2ca035f54ad2529bbe3bf94db2fc66983fc9c8e7
EDGI9 <dias.s.edgar@gmail.com>
2024-10-04 09:38:40 +0000

    Creation of base Footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
 
   return (
-    <div className="w-full h-full pb-6 bg-main-background bg-no-repeat bg-[center_top_45em]">
+    <div className="w-full h-full bg-main-background bg-no-repeat bg-[center_top_45em]">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
