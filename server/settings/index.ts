86d0c2150b1249e50a1f760a3c68728c1b700635
EDGI9 <dias.s.edgar@gmail.com>
2025-03-04 14:38:18 +0000

    Updating server configuration for fixing CORS issue with web application

--- server/settings/index.ts
+++ server/settings/index.ts
@@ -1,8 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+const PORT = 5000;
+
 export default {
     dbConnectionString: process.env.DATABASE_URI || 'mongodb://mongo:27017',
-    port: process.env.PORT || 5000,
+    port: process.env.PORT || PORT,
     useMockDatabase: process.env.USE_MOCK_DATABASE === 'true' || false,
+    origin: process.env.FRONTEND_URL || `http://localhost:${PORT}`,
 };
